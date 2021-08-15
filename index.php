160364c055288d98e82fd623f460253a70de953c
sayaka-tamura <arccycle@gmail.com>
2021-08-15 14:04:48 +0900

    add <br> tag for each sentences

--- index.php
+++ index.php
@@ -2,15 +2,18 @@
   $message = "Hello World!!!!";
   $message = "Good evening!";
   $name = "田中";
-  echo "{$message}"." "."{$name} さん!\n\n";
+  echo "{$message}"." "."{$name} さん!"."<br>";
 
 
   $a = "合計金額は";
   $a .= 2000;
-  $a .= "円です\n";
+  $a .= "円です";
+  $a .= "<br>";
   echo $a;
 
   //[123.456] を小数点第一位以下で四捨五入する
   $b = round(123.456, 1);
   echo $b;
+
+
 ?>
