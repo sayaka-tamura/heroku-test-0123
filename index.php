e6a884c3f537a0d83a8b49013e3100ccb966c074
sayaka-tamura <arccycle@gmail.com>
2021-08-15 13:50:07 +0900

    add fee of sum;

--- index.php
+++ index.php
@@ -2,5 +2,12 @@
   $message = "Hello World!!!!";
   $message = "Good evening!";
   $name = "田中";
-  echo "{$message} + {$name} さん!";
+  echo "{$message}, {$name} さん!";
+  echo "\n\n";
+
+
+  $a = "合計金額は";
+  $b = 2000;
+  $c = "円です\n";
+  echo $a.$b.$c;
 ?>
